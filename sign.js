28fdd49cb55db7f5e968cfd7f2a10fd499ef5a55
Tom Guimberteau <tomguimberteau@gmail.com>
2018-02-23 22:25:01 -0500

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,42 +1,20 @@
 const sign = require('keybase-sign')
+var fs = require('fs');
+var fs = require('fs');
 
-const privateKey =  `ENTER PGP PRIVATE KEY BLOCK HERE`
+var key = fs.readFileSync('key.txt');
+var privatekey = key.toString('ascii');
 
-const message = 'ENTER MESSAGE HERE'
-const passphrase = 'ENTER PASSWORD HERE' // Only required if private key is locked
+var pass = fs.readFileSync('password.txt');
+var password = pass.toString('ascii');
 
-function hex2a(hexx) {
-    var hex = hexx.toString();
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
-
-function a2hex(str) {
-  var arr = [];
-  for (var i = 0, l = str.length; i < l; i ++) {
-    var hex = Number(str.charCodeAt(i)).toString(16);
-    arr.push(hex);
-  }
-  return arr.join('');
-}
+var im = fs.readFileSync('file.jpeg');
+var image = im.toString('hex');
 
 try {
-  sign(privateKey, message, passphrase).then(detachedSignature => {
-    console.log(detachedSignature)
-    absolute = a2hex(detachedSignature) //convert to hex
-    console.log('0x' + absolute) //convert to hex
-
-    /*
-    -----BEGIN PGP MESSAGE-----
-    Version: Keybase OpenPGP v2.0.68
-    Comment: https://keybase.io/crypto
-
-    yMCQAnicAUQBu/7EDQMACgFK7D7a1hCEhQHLFHUAWN58I1RoaXMgaXMgYSB0ZXN0
-    ...
-    -----END PGP MESSAGE-----
-    */
+  sign(privatekey, image, password).then(sig => {
+    var sig = sig
+    console.log(sig)
   })
 } catch(err) {
     console.log('There was an error signing', err)
