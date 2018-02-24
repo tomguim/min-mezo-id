2221b373ab6cace405fc706f307953115a2044e7
Tom Guimberteau <tomguimberteau@gmail.com>
2018-02-23 22:28:15 -0500

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,19 +1,28 @@
 const sign = require('keybase-sign')
 var fs = require('fs');
 
-var e = fs.readFileSync('key.txt');
-var a = e.toString('ascii');
+var key = fs.readFileSync('key.txt');
+var privatekey = key.toString('ascii');
 
-var f = fs.readFileSync('file.jpeg');
-var b = f.toString('hex');
+var pass = fs.readFileSync('password.txt');
+var password = pass.toString('ascii');
 
-var g = fs.readFileSync('password.txt');
-var c = g.toString('ascii');
+var im = fs.readFileSync('file.jpeg');
+var image = im.toString('hex');
 
 try {
-  sign(a, b, c).then(d => {
-    var d = d
-    console.log(d)
+  sign(privatekey, image, password).then(sig => {
+    var sig = sig
+    console.log(sig)
+    /*
+    -----BEGIN PGP MESSAGE-----
+    Version: Keybase OpenPGP v2.0.68
+    Comment: https://keybase.io/crypto
+
+    yMCQAnicAUQBu/7EDQMACgFK7D7a1hCEhQHLFHUAWN58I1RoaXMgaXMgYSB0ZXN0
+    ...
+    -----END PGP MESSAGE-----
+    */
   })
 } catch(err) {
     console.log('There was an error signing', err)
